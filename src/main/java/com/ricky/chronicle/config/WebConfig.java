7f8ed1e0ed99e9189353bd39778180f7fa85a894
Ricky <tcchan71@gmail.com>
2026-02-15 16:48:08 +0000

    created frontend, deleted because of large objects, have to redo delete endpoints

--- src/main/java/com/ricky/chronicle/config/WebConfig.java
+++ src/main/java/com/ricky/chronicle/config/WebConfig.java
@@ -1,15 +1,29 @@
 package com.ricky.chronicle.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+            .allowedOrigins("http://localhost:5173")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            .allowedHeaders("*") 
+            .allowCredentials(true);
+    }
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**")
-				.addResourceLocations("classpath:/static/");
+		registry.addResourceHandler("/**")
+				.addResourceLocations("classpath:/static/")
+				.setCachePeriod(3600) 
+                .resourceChain(true)
+                .addResolver(new PathResourceResolver());
+				
 	}
 }
--- src/main/java/com/ricky/chronicle/controller/BaseController.java
+++ src/main/java/com/ricky/chronicle/controller/BaseController.java
@@ -3,11 +3,29 @@ package com.ricky.chronicle.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class BaseController {
-    @GetMapping({"/","/api"})
-    public String index(){
-        return "forward:/resources/index.html";
+    @GetMapping(value = { 
+        "/", 
+        "/{path:[^\\.]*}", 
+        "/**/{path:[^\\.]*}",
+        "/**/{path:(?!api)[^\\.]*}"
+    })
+    public String redirect(HttpServletRequest request) {
+        String path = request.getRequestURI();
+        if (path.equals("/api") || path.equals("/api/")) {
+        return "forward:/index.html";
+        }
+        if (path.startsWith("/api/") && path.length() > 5) {
+        return null; 
+        }
+        if (path.contains(".")) {
+        return null;
+        }
+        
+        return "forward:/index.html";
     }
 }
